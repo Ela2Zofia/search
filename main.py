aa7bd6d0e756b86e901005bcaf541bee66c28213
Ela2Zofia <53170511+Ela2Zofia@users.noreply.github.com>
2021-03-23 20:27:36 +0800

    Added finish condition & handle input

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ This script contains the entry point to the program (the code in
 
 import sys
 import json
+from collections import defaultdict
 
 # If you want to separate your code into separate files, put them
 # inside the `search` directory (like this one and `util.py`) and
@@ -21,10 +22,48 @@ def main():
     except IndexError:
         print("usage: python3 -m search path/to/input.json", file=sys.stderr)
         sys.exit(1)
-
+    
     # TODO:
     # Find and print a solution to the board configuration described
     # by `data`.
     # Why not start by trying to print this configuration out using the
     # `print_board` helper function? (See the `util.py` source code for
-    # usage information).
+    # usage information)
+
+    board_dict = defaultdict([])
+    for category in data:
+        for token in data[category]:
+            if token:
+                if category == "upper":
+                    board_dict[(token[1], token[2])].append( "(" + token[0].upper()   + ")" )
+                else:
+                    board_dict[(token[1], token[2])].append( "(" + token[0] + ")" )
+    
+    finished = False
+
+    while not finished:
+        finished = True
+        
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        for value in board_dict.values():
+            if value.isupper():
+                finished = False
+
+    
+    print_board(board_dict, compact=False)
